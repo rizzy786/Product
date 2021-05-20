1f6304c7d13b6d3ede096b61f8c600ec31811899
rizzy_333 <r.mansoor2636@student.leedsbeckett.ac.uk>
2021-05-20 16:46:40 +0100

    styling homepage

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,27 @@
 @extends('layouts.appold')
-@section('content') 
+@section('content')
 
-<img src = "xr-banner.jpg" width = "100%" />
+    <body>
+        <main>
+            <section class="presentation">
+                <div class="introduction">
+                    <div class="intro-text">
+                        <h1>Desktop for the Future</h1>
+                        <p>
+                            27.5" 4K UHD Display
+
+                        </p>
+                    </div>
+                    <div class="cta">
+                        <a href="{{ url('/product') }}" class="btn btn-primary btn-lg">View more Products</a>
+                        <button class="cta-add">Add To Cart</button>
+                    </div>
+                </div>
+                <div class="cover">
+                    <img src="./image/desktop.jpg"/>
+                </div>
+            </section>
+        </main>
+    </body>
 
 @endsection
